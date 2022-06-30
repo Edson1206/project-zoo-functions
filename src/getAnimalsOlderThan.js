60538ac5f5a2cdaf0507595cab92f5b1274b68ef
Edson Pereira <ed.1206@gmail.com>
2022-06-29 21:56:28 -0300

    Resolvido requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const bicho = data.species.filter((specie) => specie.name === animal)[0].residents;
+  return bicho.every((idade) => idade.age >= age);
 }
-
+console.log(getAnimalsOlderThan('lions'));
 module.exports = getAnimalsOlderThan;
