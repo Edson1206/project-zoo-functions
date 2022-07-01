d4c2457063864395e01367931f1778c8c09830d9
Edson Pereira <ed.1206@gmail.com>
2022-06-30 23:29:31 -0300

    Resolvido requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const animalUndefined = species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return (acc);
+    }, {});
+    return animalUndefined;
+  }
+  const animals = species.find((element) => element.name === animal.specie).residents;
+  if (animal.sex) {
+    return animals.filter((specie) => specie.sex === animal.sex).length;
+  }
+  return animals.length;
 }
 
 module.exports = countAnimals;
