13a5cfd433aefddadf9c2d0ffd3187c712f41a47
Edson Pereira <ed.1206@gmail.com>
2022-07-01 17:20:21 -0300

    Resolvido requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach(({ age }) => {
+    if (age < 18) obj.child += 1;
+    else if (age < 50 && age >= 18) obj.adult += 1;
+    else obj.senior += 1;
+  });
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.values(entrants).length === 0) return 0;
+  const ages = countEntrants(entrants);
+  const { child, adult, senior } = ages;
+  const priceAdult = data.prices.adult;
+  const priceChild = data.prices.child;
+  const priceSenior = data.prices.senior;
+  const totalValue = (child * priceChild) + (adult * priceAdult) + (senior * priceSenior);
+  return totalValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
