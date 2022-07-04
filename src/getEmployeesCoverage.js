cb0094db2aea10ba99ae567c006f320f78cf60e9
Edson Pereira <ed.1206@gmail.com>
2022-07-04 18:08:06 -0300

    Resolvido requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getAllEmployees() {
+  return employees
+    .map((item) => ({
+      id: item.id,
+      fullName: `${item.firstName} ${item.lastName}`,
+      species: item.responsibleFor.map((element) =>
+        species.find(({ id }) => id === element).name),
+      locations: item.responsibleFor.map((animalId) =>
+        species.find(({ id }) => id === animalId).location),
+    }));
+}
+
+function getEmployee(employee) {
+  const allEmployees = getAllEmployees();
+  const employeeData = allEmployees
+    .find((item) => item.fullName.includes(employee.name) || item.id === employee.id);
+  if (!employeeData) {
+    throw new Error('Informações inválidas');
+  }
+  return employeeData;
+}
+
+function getEmployeesCoverage(employee) {
+  if (employee && (employee.name || employee.id)) {
+    return getEmployee(employee);
+  }
+  return getAllEmployees();
 }
 
 module.exports = getEmployeesCoverage;
